9cf5dd7957af320529f60d9fc5292f0342a476ae
Have Samuel <samiehave9@gmail.com>
2022-12-09 01:56:31 +0300

    Adding custom function

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,8 +1,16 @@
 const defaultValue = 0;
 let currentResult = defaultValue;
 
+function add(x, y) {
+  const result = x + y;
+  alert('The result is: ' +  result);
+}
+add(3, 6);
+add(5, 6);
+
 currentResult += 4 + 5 / (3 * 2 - 1);
 const description = `(${defaultValue} + 4 + 5 / (3 * 2 - 1)`;
-const errorMessage = 'AnotherTxt! \n '  + ' Whehter';
 
-outputResult(currentResult, errorMessage);
+outputResult(currentResult, description);
+
+addBtn.addEventListener('click', add);
