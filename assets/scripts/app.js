46168e78ea2a7a5c940a7fc68e595b6515169007
Have Samuel <samiehave9@gmail.com>
2022-12-09 01:15:08 +0300

    More on strings

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -2,6 +2,7 @@ const defaultValue = 0;
 let currentResult = defaultValue;
 
 currentResult += 4 + 5 / (3 * 2 - 1);
-const description = '(' + defaultValue + ' + 4 + 5 / (3 * 2 - 1)';
+const description = `(${defaultValue} + 4 + 5 / (3 * 2 - 1)`;
+const errorMessage = 'AnotherTxt! \n '  + ' Whehter';
 
-outputResult(currentResult, description);
+outputResult(currentResult, errorMessage);
