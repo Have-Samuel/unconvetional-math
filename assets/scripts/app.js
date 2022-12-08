8e6265a1f376b33817bbecd1fa5e84be5514d3b1
Have Samuel <samiehave9@gmail.com>
2022-12-09 02:09:44 +0300

    Using the Return statement

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -3,12 +3,10 @@ let currentResult = defaultValue;
 
 function add(x, y) {
   const result = x + y;
-  alert('The result is: ' +  result);
+  return result;
 }
-add(3, 6);
-add(5, 6);
+currentResult = add(3, 6);
 
-currentResult += 4 + 5 / (3 * 2 - 1);
 const description = `(${defaultValue} + 4 + 5 / (3 * 2 - 1)`;
 
 outputResult(currentResult, description);
